0dd84025ca960a4500bc5671177a03f3af1bd893
ASUS <oday.aw97@gmail.com>
2021-01-15 15:46:43 +0200

    Some update to ChatRoomPage

--- lib/pages/ChatRoom/ChatRoomPage.dart
+++ lib/pages/ChatRoom/ChatRoomPage.dart
@@ -1,3 +1,5 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:stumeapp/Models/Group.dart';
@@ -6,7 +8,6 @@ import 'package:stumeapp/Models/MyUser.dart';
 import 'package:stumeapp/Models/User.dart';
 import 'package:stumeapp/controller/AuthController.dart';
 import 'package:stumeapp/controller/ChatController.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatRoomPage extends StatefulWidget {
   String id_user;
@@ -62,10 +63,13 @@ class _ChatRoomPageState extends State<ChatRoomPage> {
           contentPadding: EdgeInsets.zero,
           onTap: () {},
           title: Text(widget.user.firstName + ' ' + widget.user.secondName),
-          leading:  ClipRRect(
+          leading: ClipRRect(
             borderRadius: BorderRadius.circular(57),
-            child:  Image.network(
-              widget.user.img,
+            child: CachedNetworkImage(
+              placeholder: (context, url) => Center(
+                child: CircularProgressIndicator(),
+              ),
+              imageUrl: widget.user.img,
               fit: BoxFit.cover,
               width: 38,
               height: 38,
@@ -79,90 +83,95 @@ class _ChatRoomPageState extends State<ChatRoomPage> {
           ? Column(
               children: [
                 Expanded(
-                    child: SingleChildScrollView(
-                  reverse: true,
-                  child: Column(
-                    children: [
-                      for (int i = messages.length - 1; i >= 0; i--)
-                        messages[i] == null
-                            ? FlatButton(
-                                onPressed: () {
-                                  getMessages();
-                                },
-                                child: Text('Load More'),
-                              )
-                            : _MessageBuilder(Message()
-                                .fromMap(messages[i].data())
-                                .setId(messages[i].id)),
-                      StreamBuilder(
-                        stream: _getMessages,
-                        builder: (_, snapshot) {
-                          if (snapshot.hasData &&
-                              snapshot.data.docs.length > 0) {
-                            print(snapshot.data.docs[0].data());
-                            newMessages = snapshot.data.docs;
-                          }
-                          return Row(
-                            children: [
-                              Container(
-                                width: size.width,
-                                child: Column(
-                                  children: [
-                                    for (var message in newMessages)
-                                      _MessageBuilder(Message()
-                                          .fromMap(message.data())
-                                          .setId(message.id)),
-                                  ],
-                                ),
-                              )
-                            ],
-                          );
-                        },
-                      ),
-                    ],
-                  ),
-                )),
-                Card(
-                  margin: EdgeInsets.zero,
-                  elevation: 0,
-                  child: Container(
-                    width: size.width,
-                    height: 64,
-                    child: Row(
+                  child: SingleChildScrollView(
+                    reverse: true,
+                    child: Column(
                       children: [
-                        Expanded(
-                            child: TextField(
-                          controller: _messageController,
-                          decoration: InputDecoration(
-                              border: InputBorder.none,
-                              contentPadding:
-                                  EdgeInsets.symmetric(horizontal: 8),
-                              hintText: 'Type a message..'),
-                        )),
-                        Padding(
-                          padding: const EdgeInsets.all(2.0),
-                          child: FloatingActionButton(
-                            elevation: 0,
-                            onPressed: () {
-                              if (_messageController.text.isEmpty) return;
-                              _chatController.addMessage(
-                                message: Message(
-                                  idOwner: _authController.getUser.uid,
-                                  text: _messageController.text,
-                                  date: DateTime.now(),
-                                ),
-                                id_chat: getChatID(),
-                              );
-                              _messageController.clear();
-                            },
-                            backgroundColor: Colors.indigo,
-                            child: Icon(Icons.send),
-                          ),
+                        for (int i = messages.length - 1; i >= 0; i--)
+                          messages[i] == null
+                              ? FlatButton(
+                                  onPressed: () {
+                                    getMessages();
+                                  },
+                                  child: Text('Load More'),
+                                )
+                              : _MessageBuilder(Message()
+                                  .fromMap(messages[i].data())
+                                  .setId(messages[i].id)),
+                        StreamBuilder(
+                          stream: _getMessages,
+                          builder: (_, snapshot) {
+                            if (snapshot.hasData &&
+                                snapshot.data.docs.length > 0) {
+                              print(snapshot.data.docs[0].data());
+                              newMessages = snapshot.data.docs;
+                            }
+                            return Row(
+                              children: [
+                                Container(
+                                  width: size.width,
+                                  child: Column(
+                                    children: [
+                                      for (var message in newMessages)
+                                        _MessageBuilder(Message()
+                                            .fromMap(message.data())
+                                            .setId(message.id)),
+                                    ],
+                                  ),
+                                )
+                              ],
+                            );
+                          },
                         ),
                       ],
                     ),
                   ),
-                )
+                ),
+                Container(
+                  width: size.width,
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          textAlign: TextAlign.start,
+                          controller: _messageController,
+                          enableSuggestions: true,
+                          // maxLines: null,
+                          // minLines: null,
+                          // expands: true,
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Colors.red, width: 5.0),
+                              ),
+                              contentPadding:
+                                  EdgeInsets.symmetric(horizontal: 8),
+                              hintText: "Type a message..."),
+                        ),
+                      ),
+                      FlatButton(
+                        child: Icon(
+                          Icons.send,
+                          color: Colors.indigo,
+                        ),
+                        onPressed: () {
+                          if (_messageController.text.isEmpty) return;
+                          _chatController.addMessage(
+                            message: Message(
+                              idOwner: _authController.getUser.uid,
+                              text: _messageController.text,
+                              date: DateTime.now(),
+                            ),
+                            id_chat: getChatID(),
+                          );
+                          _messageController.clear();
+                        },
+                      ),
+                    ],
+                  ),
+                ),
               ],
             )
           : Center(child: CircularProgressIndicator()),
@@ -230,90 +239,130 @@ class _ChatRoomPageState extends State<ChatRoomPage> {
   }
 
   _MessageBuilder(Message message) {
-    if (message.idOwner == _authController.getUser.uid)
+    if (message.idOwner == _authController.getUser.uid) {
+      //Start my message
       return Container(
         margin: EdgeInsets.all(8),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.end,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Container(
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(0),
-                      bottomLeft: Radius.circular(20),
-                      bottomRight: Radius.circular(20),
-                      topLeft: Radius.circular(20),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(0),
+                        bottomLeft: Radius.circular(20),
+                        bottomRight: Radius.circular(20),
+                        topLeft: Radius.circular(20),
+                      ),
+                      color: Colors.black38,
                     ),
-                    color: Colors.deepOrange,
+                    child: Text(
+                      message.text,
+                      textAlign: TextAlign.justify,
+                      // overflow: TextOverflow.ellipsis,
+                      style: TextStyle(color: Colors.white),
+                    ),
                   ),
-                  child: Text(
-                    message.text,
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ],
+                  //TODO add message time
+                  // Text(
+                  //   message.date.toString(),
+                  //   textAlign: TextAlign.justify,
+                  //   maxLines: 100,
+                  //   // overflow: TextOverflow.ellipsis,
+                  //   style: TextStyle(color: Colors.red),
+                  // ),
+                ],
+              ),
             ),
             SizedBox(
               width: 5,
             ),
             ClipRRect(
               borderRadius: BorderRadius.circular(57),
-              child:  Image.network(
-                MyUser.myUser.img,
-                fit: BoxFit.cover,
-                width: 30,
-                height: 30,
+              //TODO hide images
+              child: 1 == 1
+                  ? CachedNetworkImage(
+                      placeholder: (context, url) => Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                      imageUrl: MyUser.myUser.img,
+                      fit: BoxFit.cover,
+                      width: 30,
+                      height: 30,
+                    )
+                  : Container(
+                      width: 30,
+                      height: 30,
+                    ),
+            )
+          ],
+        ),
+      );
+      //End my message
+
+    } else {
+      //Start other's message
+      return Container(
+        margin: EdgeInsets.all(8),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(57),
+              //TODO hide images
+              child: 1 == 1
+                  ? CachedNetworkImage(
+                      placeholder: (context, url) => Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                      imageUrl: widget.user.img,
+                      fit: BoxFit.cover,
+                      width: 30,
+                      height: 30,
+                    )
+                  : Container(
+                      width: 30,
+                      height: 30,
+                    ),
+            ),
+            SizedBox(
+              width: 5,
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(20),
+                        bottomLeft: Radius.circular(20),
+                        bottomRight: Radius.circular(20),
+                        topLeft: Radius.circular(0),
+                      ),
+                      color: Colors.black54,
+                    ),
+                    child: Text(
+                      message.text,
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
         ),
       );
-    return Container(
-      margin: EdgeInsets.all(8),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(57),
-            child:  Image.network(
-              widget.user.img,
-              fit: BoxFit.cover,
-              width: 30,
-              height: 30,
-            ),
-          ),
-          SizedBox(
-            width: 5,
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(20),
-                    bottomLeft: Radius.circular(20),
-                    bottomRight: Radius.circular(20),
-                    topLeft: Radius.circular(0),
-                  ),
-                  color: Colors.deepOrange[300],
-                ),
-                child: Text(
-                  message.text,
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
+      //End other's message
+    }
   }
 }
