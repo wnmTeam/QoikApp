9afc4e9933dd13b2bf745a04b5fe23e7ad66396b
ASUS <oday.aw97@gmail.com>
2021-02-02 07:45:53 +0200

    Icons update

--- lib/pages/Home/HomePage.dart
+++ lib/pages/Home/HomePage.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:stumeapp/Models/MyUser.dart';
 import 'package:stumeapp/Models/User.dart';
 import 'package:stumeapp/api/notification_api.dart';
@@ -10,8 +12,6 @@ import 'package:stumeapp/pages/Home/tabs/GroupsChatsTabView.dart';
 import 'package:stumeapp/pages/Home/tabs/HomeTabView.dart';
 import 'package:stumeapp/pages/Home/tabs/LibraryTabView.dart';
 import 'package:stumeapp/pages/Home/widgets/FABwithBottomAppBar.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -46,7 +46,7 @@ class _HomePageState extends State<HomePage> {
 
   _getUserInfo() async {
     DocumentSnapshot d =
-        await _authController.getUserInfo(_authController.getUser.uid);
+    await _authController.getUserInfo(_authController.getUser.uid);
     User user = User().fromMap(d.data()).setId(d.id);
     MyUser.myUser = user;
     _authController.updateUserTag(user);
@@ -107,42 +107,42 @@ class _HomePageState extends State<HomePage> {
             padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 0),
             child: !loading
                 ? InkWell(
-                    onTap: () {
-                      Navigator.of(context).pushNamed(
-                        '/ProfilePage',
-                        arguments: {
-                          'user': MyUser.myUser,
-                          'id_user': _authController.getUser.uid,
-                        },
-                      );
-                    },
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(20),
-                          child: Image.network(
-                            MyUser.myUser.img != null ? MyUser.myUser.img : ' ',
-                            fit: BoxFit.cover,
-                            width: 40,
-                            height: 40,
-                          ),
-                        ),
-                      ],
+              onTap: () {
+                Navigator.of(context).pushNamed(
+                  '/ProfilePage',
+                  arguments: {
+                    'user': MyUser.myUser,
+                    'id_user': _authController.getUser.uid,
+                  },
+                );
+              },
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.network(
+                      MyUser.myUser.img != null ? MyUser.myUser.img : ' ',
+                      fit: BoxFit.cover,
+                      width: 40,
+                      height: 40,
                     ),
-                  )
-                : Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ClipRRect(
-                          borderRadius: BorderRadius.circular(20),
-                          child: Container(
-                            width: 40,
-                            height: 40,
-                            color: Colors.white,
-                          )),
-                    ],
                   ),
+                ],
+              ),
+            )
+                : Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Container(
+                      width: 40,
+                      height: 40,
+                      color: Colors.white,
+                    )),
+              ],
+            ),
           ),
         ],
       ),
@@ -157,11 +157,11 @@ class _HomePageState extends State<HomePage> {
             text: 'Home',
           ),
           FABBottomAppBarItem(
-            iconData: Icons.chat_rounded,
+            iconData: Icons.group,
             text: 'Groups',
           ),
           FABBottomAppBarItem(
-            iconData: Icons.group,
+            iconData: Icons.person,
             text: 'Friends',
           ),
           FABBottomAppBarItem(
@@ -223,27 +223,23 @@ class _HomePageState extends State<HomePage> {
               ListTile(
                 title: Text('Setting'),
                 leading: Icon(Icons.settings),
-                onTap: () {
-                },
+                onTap: () {},
               ),
               Divider(),
               ListTile(
                 title: Text('About'),
                 leading: Icon(Icons.warning_rounded),
-                onTap: () {
-                },
+                onTap: () {},
               ),
               ListTile(
                 title: Text('Contact Us'),
                 leading: Icon(Icons.group),
-                onTap: () {
-                },
+                onTap: () {},
               ),
               ListTile(
                 title: Text('FAQ'),
                 leading: Icon(Icons.help),
-                onTap: () {
-                },
+                onTap: () {},
               ),
               Divider(),
               ListTile(
@@ -262,8 +258,8 @@ class _HomePageState extends State<HomePage> {
       body: !loading
           ? tabViews[_currentIndex]
           : Center(
-              child: CircularProgressIndicator(),
-            ),
+        child: CircularProgressIndicator(),
+      ),
     );
   }
 
