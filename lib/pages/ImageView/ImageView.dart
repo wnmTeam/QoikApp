62c5acb12e8f36ed0ad0203c1e994b7e99e11541
ASUS <oday.aw97@gmail.com>
2021-02-05 10:30:50 +0200

    Update imagePreview

--- lib/pages/ImageView/ImageView.dart
+++ lib/pages/ImageView/ImageView.dart
@@ -13,6 +13,7 @@ class ImageView extends StatefulWidget {
 class _ImageViewState extends State<ImageView> {
   String url;
   double width;
+  bool showAppBar = true;
 
   _ImageViewState(this.url);
 
@@ -20,45 +21,53 @@ class _ImageViewState extends State<ImageView> {
   Widget build(BuildContext context) {
     width = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-      ),
-      body: Container(
-        child: PhotoView(
-          imageProvider: NetworkImage(url),
-          enableRotation: true,
-          heroAttributes: PhotoViewHeroAttributes(tag: url),
-          minScale: PhotoViewComputedScale.contained,
-          errorBuilder: (context, error, stackTrace) => Center(
-            child: Icon(
-              Icons.error_outline,
-              color: Colors.red,
-              size: width / 2,
-            ),
-          ),
-          loadFailedChild: Center(
-            child: Icon(
-              Icons.error_outline,
-              color: Colors.red,
-              size: width / 2,
-            ),
-          ),
-          loadingBuilder: (context, event) => Padding(
-            padding: EdgeInsets.all(50),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                CircularProgressIndicator(),
-                LinearProgressIndicator(
-                  value: event == null
-                      ? 0
-                      : event.cumulativeBytesLoaded / event.expectedTotalBytes,
-                )
-              ],
-            ),
+      extendBodyBehindAppBar: true,
+      appBar: showAppBar
+          ? AppBar(
+              backgroundColor: Colors.transparent,
+            )
+          : null,
+      body: PhotoView(
+        backgroundDecoration: BoxDecoration(
+          color: Colors.white,
+        ),
+        imageProvider: NetworkImage(url),
+        enableRotation: true,
+        heroAttributes: PhotoViewHeroAttributes(tag: url),
+        minScale: PhotoViewComputedScale.contained,
+        errorBuilder: (context, error, stackTrace) => Center(
+          child: Icon(
+            Icons.error_outline,
+            color: Colors.red,
+            size: width / 2,
           ),
         ),
-        color: Colors.white,
+        loadFailedChild: Center(
+          child: Icon(
+            Icons.error_outline,
+            color: Colors.red,
+            size: width / 2,
+          ),
+        ),
+        loadingBuilder: (context, event) => Padding(
+          padding: EdgeInsets.all(50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              CircularProgressIndicator(),
+              LinearProgressIndicator(
+                value: event == null
+                    ? 0
+                    : event.cumulativeBytesLoaded / event.expectedTotalBytes,
+              )
+            ],
+          ),
+        ),
+        onTapUp: (context, details, controllerValue) {
+          setState(() {
+            showAppBar = !showAppBar;
+          });
+        },
       ),
     );
   }
