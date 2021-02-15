820101a82d29fade4555d5b7bbb33568d71caaf3
ASUS <oday.aw97@gmail.com>
2021-02-15 05:48:33 +0200

    Update FloatingActionButton location

--- lib/pages/Group/tabs/PostsTabView.dart
+++ lib/pages/Group/tabs/PostsTabView.dart
@@ -1,10 +1,10 @@
 import 'dart:async';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:stumeapp/Models/Group.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:stumeapp/Models/Post.dart';
 import 'package:stumeapp/const_values.dart';
 import 'package:stumeapp/controller/AuthController.dart';
@@ -171,6 +171,7 @@ class _PostsTabState extends State<PostsTab>
               ),
             )
           : null,
+      floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
     );
   }
 
