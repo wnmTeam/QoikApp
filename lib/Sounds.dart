d835c9a5dfec0d8ad8d5bff072a832f24981f2db
ASUS <oday.aw97@gmail.com>
2021-02-25 11:06:07 +0200

    Update sounds

--- lib/Sounds.dart
+++ lib/Sounds.dart
@@ -6,27 +6,32 @@ class Sounds {
 
   Future<AudioPlayer> sendMessageSound() async {
     AudioCache cache = new AudioCache();
-    return await cache.play("hat.wav");
+    return await cache.play("message_out.wav");
   }
 
-  Future<AudioPlayer> commentSound() async {
+  Future<AudioPlayer> inMessageSound() async {
     AudioCache cache = new AudioCache();
-    return await cache.play("hat.wav");
+    return await cache.play("message_in.wav");
   }
 
   Future<AudioPlayer> likeSound() async {
     AudioCache cache = new AudioCache();
-    return await cache.play("hat.wav");
+    return await cache.play("like.ogg");
   }
 
   Future<AudioPlayer> disLikeSound() async {
     // AudioCache cache = new AudioCache();
-    // return await cache.play("hat.wav");
+    // return await cache.play("disLike.wav");
+  }
+
+  Future<AudioPlayer> commentSound() async {
+    AudioCache cache = new AudioCache();
+    return await cache.play("comment.ogg");
   }
 
   Future<AudioPlayer> postSound() async {
     AudioCache cache = new AudioCache();
-    return await cache.play("hat.wav");
+    return await cache.play("post.wav");
   }
 
   dispose() {
