fabe2b80e98275cb7166d16448d87c5df65a78db
ASUS <oday.aw97@gmail.com>
2021-03-16 12:22:24 +0200

    Update UserPlaceHolder

--- lib/pages/widgets/UserPlaceholder.dart
+++ lib/pages/widgets/UserPlaceholder.dart
@@ -1,37 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+import 'package:stumeapp/main.dart';
 
 class UserPlaceholder extends StatelessWidget {
+  final Color baseColor = MyAppState.isDark ? Colors.black87 : Colors.grey[100];
+  final Color highlightColor =
+      MyAppState.isDark ? Colors.black54 : Colors.grey[300];
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
       contentPadding: EdgeInsets.zero,
-      leading: ClipRRect(
-          borderRadius: BorderRadius.circular(57),
-          child: Container(
-            color: Colors.grey[200],
-            width: 57,
-            height: 57,
-          )),
+      leading: Shimmer.fromColors(
+        baseColor: baseColor,
+        highlightColor: highlightColor,
+        child: ClipRRect(
+            borderRadius: BorderRadius.circular(57),
+            child: Container(
+              color: Colors.grey,
+              width: 57,
+              height: 57,
+            )),
+      ),
       title: Row(
         children: [
-          ClipRRect(
-              borderRadius: BorderRadius.circular(57),
-              child: Container(
-                color: Colors.grey[200],
-                width: 150,
-                height: 20,
-              )),
+          Shimmer.fromColors(
+            baseColor: baseColor,
+            highlightColor: highlightColor,
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(57),
+                child: Container(
+                  color: Colors.grey,
+                  width: 150,
+                  height: 20,
+                )),
+          ),
         ],
       ),
       subtitle: Row(
         children: [
-          ClipRRect(
-              borderRadius: BorderRadius.circular(57),
-              child: Container(
-                color: Colors.grey[200],
-                width: 70,
-                height: 10,
-              )),
+          Shimmer.fromColors(
+            baseColor: baseColor,
+            highlightColor: highlightColor,
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(57),
+                child: Container(
+                  color: Colors.grey,
+                  width: 70,
+                  height: 10,
+                )),
+          )
         ],
       ),
     );
